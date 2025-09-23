5bc5a6346f1cf14356edc522045f958c7cd7a0db
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2025-09-23 15:35:30 -0500

    Agregar script .bat para iniciar frontend y backend de Vitri

---
+++ .bat.bat
@@ -0,0 +1,16 @@
+@echo off
+title Vitri - Frontend & Backend
+
+REM Frontend (Vite)
+cd /d "%~dp0vitriFrontend-semillero"
+start /min "Frontend" cmd /k "npm run dev"
+
+REM Abrir navegador automáticamente
+start "" http://localhost:5173/
+
+REM Backend (Flask)
+cd /d "%~dp0vitriBackend-semillero"
+call .venv\Scripts\activate
+cd app
+set FLASK_APP=app.py
+start /min "Backend" cmd /k "flask run --debug"
